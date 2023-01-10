9f0553452e5e178f063782bb99a2a8c869191340
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2023-01-10 16:42:38 +0100

    commit inicial

---
+++ src/main/java/daw/programacion/exceptions/App.java
@@ -0,0 +1,13 @@
+package daw.programacion.exceptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
