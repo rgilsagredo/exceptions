eed454a5cb6c68f30d4d9558d5e6acb0b0e19233
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2023-01-13 15:49:48 +0100

    falta poco

--- src/main/java/daw/programacion/exceptions/App.java
+++ src/main/java/daw/programacion/exceptions/App.java
@@ -1,13 +1,219 @@
 package daw.programacion.exceptions;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.FileReader;
+
+public class App {
+    public static void main(String[] args) {
+        /*
+         * 
+         * Puede ocurrir que al correr un programa los métodos fallen. De hecho se ha
+         * visto mil veces con los arrays: aunque el código, antes de ser ejecutado,
+         * está en principio bien, a la hora de la ejecución nos encontramos con un
+         * indexOutOfBounds que no hemos previsto y el programa finaliza erronaemane
+         */
+
+        // ejemplo de un error en tiempo de ejecución
+
+        // int[] array = {1,2,3};
+        // System.out.println(array[5]); // de primeras no vemos que haya ningún fallo
+        // en la escritura del programa, pero este código no ejecuta
+
+        /*
+         * 
+         * Lo errores que ocurren en la ejecución del programa alterando el flujo normal
+         * se llama excepciones. El llevar un buen control de excepciones permite:
+         * - separar código que controla excepciones del resto (que tendrá otro
+         * propósito)
+         * - propagar errores sobre una pila de llamadas
+         * - agrupar diferentes tipos de errores y proponer su tratamiento
+         */
+
+        /*
+         * 
+         * Esecenarios típicos en los que ocurren excepciones:
+         * - el usuario de la aplicación mete datos incorrectos (invalidArgument)
+         * - se intenta abrir un fichero que no existe (IOException)
+         * - Problema de conexión (de red, a una DB)
+         * - la JVM se queda sin memoria
+         * 
+         * En estos escenarios se ven las 3 categorías de excepciones que tiene java:
+         * por error del usuario, por error del programador, o por causas externas
+         * 
+         */
+
+        /**
+         * 
+         * Excepciones en Java. Hay 3, checked, unchecked, y error.
+         * OJO: todas las excepciones son OBJETOS (instancias de una clase) que
+         * contienen información de lo que ha ido mal
+         * 
+         * como las excepciones son objetos, vienen de clases (que van heredando de
+         * otras clases, es decir, se van
+         * especializando, es decir, controlan cosas mas finas. Ejemplo: Si pillo un
+         * error de que no se puede leer un
+         * fichero, es particular estoy pillando que el fichero no existe)
+         * 
+         * Checked: son las excepciones que estamos obligados a anticipar y controlar.
+         * De hecho, nos obliga el compilador
+         * (es decir, yo no puedo hacer un build de mi aplicación sin haber controlado
+         * esta excepción).
+         * listado de excepciones comunes:
+         * https://www.codejava.net/java-core/exception/java-checked-and-unchecked-exceptions
+         * 
+         * Ejemplo clasico de esta excepción es al intentar leer un fichero (puede que
+         * el fichero no exista --
+         * por habe escrito mal el nombre, porque se ha borrado...)
+         * 
+         */
+
+        // código de una checked exception
+        // String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+        // File file = new File(pathToFile);
+        // FileReader fr = new FileReader(file);
+
+        /**
+         * Si intentamos compilar el código de arriba nos tiene que explotar en la cara,
+         * pues el propio compilador dirá
+         * que tenemos una posible excepcion sin manejar. Tiene sentido, pues el
+         * compiler se adelanta a lo dicho antes:
+         * que el fichero (por las razonas que sean) no exista, lo cual haría que
+         * nuestro programa terminase
+         * abruptamente. Es nuestro problema como programadores tener esta situación en
+         * la cabeza y darle solución,
+         * es decir, en caso de que al ir a leer el fichero efectivamente no exista,
+         * ¿qué tiene que hacer la aplicación?
+         */
+
+        /**
+         * En general, hay que preocuparse "poco" por las excepciones checked porque nos
+         * avisa el compilador
+         * de que las tenemos ahí y que hay que darles un manejo. Suelen tener que ver
+         * siempre con
+         * actividades de I/O, conexiones a DBs, no poder hacer ciertas acciones (ya
+         * vimos que para poder clonar
+         * un objeto hay que implementar un método y una interfaz, sin ambos, tenemos
+         * una checked exception).
+         * 
+         * Un poco más adelante lo vemos con más detalle, pero las excepciones tienen
+         * una jerarquía (de lo más general, es decir *alguna* exception, a cosas más
+         * concretas,
+         * por ejemplo: no existe el fichero).
+         * Pues en la jerarquía de las exceptions,
+         * las Checked son las que están bajo java.lang.Exception y NO bajo
+         * java.lang.RuntimeException.
+         * Ver arbol de jerarquías aquí:
+         * https://docs.oracle.com/en/java/javase/16/docs/api/java.base/java/lang/package-tree.html
+         * 
+         */
+
+        /**
+         * UNCHECKED Exceptions -- son las que ocurren en tiempo de ejecucicón, es
+         * decir, el
+         * compilador no las pilla. Son bugs, error el la lógica del programa, usar una
+         * API mal...
+         * Son errores de programación, y es tu problema evitar que ocurran. Es decir,
+         * si en tu programa
+         * te lanza un IndexOutOfBounds, tú tienes que buscarte la vida para que ese
+         * error no ocurra,
+         * no vale poner un try-catch. La manera de evitar estas cosas es programar bien
+         * y hacer tests.
+         * 
+         * En la jerarquía de las excepciones, las unckecked son todas las que están por
+         * debajo de RuntimeException.
+         * Las más típicas son:
+         * - NullPointerException -- hemos pasado un objeto que no existe
+         * - ArithmeticException -- dividir por cero
+         * - IllegalArgumentException -- pasas un parámetro inválido
+         * - IndexOutOfBounds -- intentar acceder a un elemento de un array que no
+         * existe
+         * - NumberFormatException -- suele ocurrir cuando intentar parsear a numero una
+         * string no numérica
+         */
+
+        // ejemplos -- null ponter
+        // String texto = null;
+        // metodo(texto);
+
+        // arithmetic
+        // System.out.println(5 / 0);
+
+        // number format
+        // int n = Integer.parseInt("asdf");
+
+        // para illegalArgument, mejor un ejemplo más adelante con la clase Cuenta,
+        // donde vamos a querer
+        // evitar que el método meterDinero acepte dinero negativo
+
+        /**
+         * Por últimmo, tienes los Error. Estos te los comes, son problemas fuera del
+         * alcance de
+         * programador o usuario. Típicamente, que la JVM se quede sin memoria.
+         * 
+         */
+
+        /**
+         * https://docs.oracle.com/en/java/javase/16/docs/api/java.base/java/lang/package-tree.html
+         * Jerarquía de las excepciones: todas vienen de Throwable, que se extiende en
+         * Error y Exception. Error son las
+         * que nos comemos, así que hay que dejar que ocurran. La clase Exception tiene
+         * a su vez
+         * muchas subclases, y hayq ue distinguir entre RuntimeException (esta clase y
+         * todas sus subclases son las UNCHECKED)
+         * y el resto de subclases (Exception y esas otras subclases son las CHECKED)
+         */
+
+        /* MANEJO DE EXCEPCIONES */
+
+        /*
+         * Hay que entender cómo funciona java --> el programa siempre entre por el
+         * main, y el main
+         * se irá encargando de llamar a otros métodos, y esos métodos a otro métodos,
+         * etc. Eso se llama la pila de
+         * llamadas (call stack). Si el program funciona bien, todos los métodos
+         * ejecutarán sin problema y el programa
+         * termina.
+         * Sin embargo, si en ejecución ocurre una excepción, la excepción se lanza
+         * (throw) desde el método que la crea
+         * hacia arriba en la pila de llamadas hasta que, o bien en algún sitio se
+         * maneje (handle), o bien el programa explote
+         * (termine lanzando un error).
+         * 
+         * En general tenemos que distinguir 2 tipos de comportamiento según las
+         * excepciones sean CHECKED o UNCHECKED.
+         * Como las CHECKED estamos obligados a manejarlas, lo que queremos es que
+         * anteuna exception checked el
+         * programa termine amablemente, o tome otras acciones, pero que no explote en
+         * la cara.
+         * 
+         * Las UNCHEKED (excepciones en runtime) NO queremos que ocurran, y sí es
+         * conveniente que nos
+         * explote el programa en la cara, para que sepamos que hay un error de
+         * programación que hay que
+         * corregir. También, a veces, queremos nosotros mismos lanzar este tipo de
+         * excepciones cuando alguien que vaya a usar nuestro programa lo esté usando
+         * de mala manera, para advertirle de que tiene que hacer las cosas bien.
+         * El ejemplo claro que ya se ha visto es el IndexOutOfBounds. Estás intentando
+         * usar la clase arrays mal, y entonces te lanza una excepcion. Es tu problema
+         * que no te lanze esa excepción, haz el código bien, no pongas un try-catch
+         * 
+         */
+
+        /**
+         * para manejar las excepciones usamos los bloques try-catch-finally, ver
+         * ejemplos concretos en
+         * las otras clases que hago
+         */
+
+        // CheckedExceptionHandling.leerFichero_v3();
+        // CheckedExceptionHandling.leerFichero_v4();
+        // System.out.println("el programa sigue");
+
+        // esto me dice que unhandled exception, porque el método pasa el marrón a otro método que lo llame
+        // CheckedExceptionHandling.leerFichero_v6();
+    }
+
+    public static void metodo(String texto) {
+        System.out.println(texto.toUpperCase());
     }
 }
---
+++ src/main/java/daw/programacion/exceptions/CheckedExceptionHandling.java
@@ -0,0 +1,154 @@
+package daw.programacion.exceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class CheckedExceptionHandling {
+
+    /* Aquí vamos a ver cómo deben manejarse excepciones checked */
+
+    /*
+     * intentamos leer un fichero (que no existe). La clase FileReader (que se
+     * encarga de leer fichero)
+     * lanza una excepcion de tipo CHECKED y por tanto nos obliga a controlarla (la
+     * propia clase
+     * ya sabe que al intentar leer un fichero podemos encontrarnos con problemas y
+     * nos obliga a lidiar
+     * con ellos)
+     */
+
+    /*
+     * Tenemos varias opciones para actuar. Si el compilador me avisa de una CHECKED
+     * exception,
+     * el código que puede explotar hay que meterlo en un bloque try (es decir,
+     * intenta hacer esto).
+     * Si el código lanza excepcion, en el momento (linea de código) en que eso
+     * ocurra, la parte del try
+     * deja de ser ejecutada, y toma el control del programa la parte del catch (si
+     * es que se coge la excepción correcta).
+     * En caso de CHECKED exceptions, el IDE te dice que tipo de excepción está
+     * lanzando, y por tnato sabes qué hay que poner en el catch.
+     * Si no sabes que te va a saltar, puedes cubrirte el culo con un Excepction
+     * (como todas las excepciones son subclases suyas, seguro que lo pilla). Pero
+     * siempre mejor
+     * ser específico, ya que te va a ayudar a solucionar los problemas antes.
+     * 
+     * NOTA: sobre cómo llamar a los objetos exception, hay gente que los llama
+     * genéricamente "ex", y otros
+     * que usan las inciales de la clase. Yo voy a usar las iniciales de la clase.
+     * 
+     * Cuando el código se ejecuta y ocurre la excecpión en el try, si el catch la
+     * coge (es deceir, le hemos
+     * dicho que pille el tipo concreto de excecpión que lanza lo del try), se
+     * ejecuta el código que hay en catch,
+     * como manera de evitar que el programa te explote en la cara. En el catch, en
+     * las excecpiones checked, puedes
+     * poner un mensae amable para el usuario de que el programa ha terminado debido
+     * a que tal o cual.
+     * Es decir, no ha ido nada dentro de nuestro (usuario y programador) control
+     * mal, simplemente ahora el
+     * código no puede correr de la manera esperada
+     * 
+     */
+
+    // este método, cuando falle el FileReader, devolverá un mensaje, y el programa
+    // terminará sin problemas
+    public static void leerFichero_v1() {
+        String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+        File file = new File(pathToFile);
+        try {
+            FileReader fr = new FileReader(file);
+            System.out.println("Esta linea de código no se ejecuta, porque tengo una excepción antes");
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("El fichero no existe");
+        }
+    }
+
+    public static void leerFichero_v2() {
+        String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+        File file = new File(pathToFile);
+        try {
+            FileReader fr = new FileReader(file);
+            System.out.println("Esta linea de código no se ejecuta, porque tengo una excepción antes");
+        } catch (Exception e) { // este es el catch pokemon: pilla todas las excepciones
+            System.out.println("El fichero no existe");
+        }
+    }
+
+    /**
+     * Mas interesante es que el objeto en el catch tiene info de que ha pasado. En
+     * general nos interesan
+     * 2 métodos, uno es e.getMessage() --> me dice que ha pasado y
+     * e.printStackTrace() --> te muestra la pila
+     * de llamadas ue llevan a la excepcion. Aquí se pueden encontrar más:
+     * https://www.tutorialspoint.com/java/java_exceptions.htm
+     */
+
+    public static void leerFichero_v3() {
+        String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+        File file = new File(pathToFile);
+        try {
+            FileReader fr = new FileReader(file);
+            System.out.println("Esta linea de código no se ejecuta, porque tengo una excepción antes");
+        } catch (Exception e) { // este es el catch pokemon: pilla todas las excepciones
+            System.out.println(e.getMessage()); // dime que ha pasado
+        }
+    }
+
+    public static void leerFichero_v4() {
+        String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+        File file = new File(pathToFile);
+        try {
+            FileReader fr = new FileReader(file);
+            System.out.println("Esta linea de código no se ejecuta, porque tengo una excepción antes");
+        } catch (Exception e) { // este es el catch pokemon: pilla todas las excepciones
+            e.printStackTrace(); // dime la pila de llamadas hasta el error. También te da el mensaje
+        }
+    } // aquí, aprendr a leer el stack de llamadas
+
+    // el programa explota si no pillamos la excepción correcta. De hecho, ni me
+    // deja compilar
+    /*
+     * public static void leerFichero_v5() {
+     * String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+     * File file = new File(pathToFile);
+     * try {
+     * FileReader fr = new FileReader(file);
+     * System.out.
+     * println("Esta linea de código no se ejecuta, porque tengo una excepción antes"
+     * );
+     * } catch (ArithmeticException e) {
+     * e.printStackTrace(); // dime la pila de llamadas hasta el error. También te
+     * da el mensaje
+     * }
+     * }
+     */
+
+    /*
+     * No siempre hay que manejar la excepción según te salga, a veces puedes
+     * pasarle el marrón a otro método
+     * Esto es porque las excepciones, según se lanzan, van hacia "arriba" en la
+     * pila de llamadas
+     * hasta que alguien se encarga de ellas
+     */
+
+     // si yo digo que este método lanza una excepción, ya se encargará otro método 
+     // de la pila que lo llame de manejar la excecpón y no tengo error de compilación (aquí)
+     // pero lo tendré en el método que llame a este
+    public static void leerFichero_v6() throws FileNotFoundException {
+        String pathToFile = "C:\\coding\\java\\clase\\exceptions\\hola.txt";
+        File file = new File(pathToFile);
+        FileReader fr = new FileReader(file);
+        System.out.println("Esta linea de código no se ejecuta, porque tengo una excepción antes");
+
+    }
+
+    /*creo que me queda: ver las uncheked -- aunque no deberían ocurrir puede ser util
+     * meter excepciones que tengan que lanzar tus métodos, el ejemplo de cuenta
+     * crear tus propias excepiones
+     * finally
+     * muliples catch
+     */
+
+}
